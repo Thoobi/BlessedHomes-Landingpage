f7c59dc7b6a5a1fc81bc4e7c51a2406749439585
Thoobi <syndeydaniel76g@gmail.com>
2025-01-23 20:01:14 +0100

    changes the blessed homes and properties to ten times better blessed homes and properties

--- src/Components/static/Footer.jsx
+++ src/Components/static/Footer.jsx
@@ -24,10 +24,11 @@ const Footer = () => {
             <p className="text-base font-medium max-lg:text-sm text-[#999999]">
               Your dream property is just a click away. Whether you&apos;re
               looking for a new home, a strategic investment, or expert real
-              estate advice, Estatein is here to assist you every step of the
-              way. Take the first step towards your real estate goals and
-              explore our available properties or get in touch with our team for
-              personalized assistance.
+              estate advice, Ten Times Better Blessed Homes and Properties is
+              here to assist you every step of the way. Take the first step
+              towards your real estate goals and explore our available
+              properties or get in touch with our team for personalized
+              assistance.
             </p>
           </div>
           <button className="text-sm w-[152px] h-[49px] font-medium bg-[#703BF7] rounded-lg">
